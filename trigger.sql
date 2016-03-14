51221d142d1b972ddccb99168913718474522836
priyankagat-webonise <gatpriyanka@gmail.com>
2016-03-14 15:04:22 +0530

    update trigger

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,8 @@
-create trigger update_user_id AFTER UPDATE on users for each row BEGIN  update customer c set c.user_id= new.user_id where c.user_id= old.user_id;  END;
+create trigger update_user_id AFTER 
+UPDATE on users 
+for each row 
+	BEGIN 
+		update customer c 
+		set c.user_id= new.user_id 
+		where c.user_id= old.user_id;
+	END;
